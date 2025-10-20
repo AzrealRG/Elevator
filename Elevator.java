8a909fdf2222eaa1f470c718ec8dee7aa0d441f5
AzrealRG <john.ha2006@gmail.com>
2025-10-20 15:11:03 -0400

    Basics of elevator

---
+++ Elevator.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Elevator{
+    private int curFloor;
+    private boolean doorOpen;
+    private Queue<Integer> floorOrder;
+    private Direction direction;
+
+    public enum Direction{
+        UP, DOWN, IDLE
+    }
+
+    //creating the elevator only requires the initial floor
+    public Elevator(int initialFloor){
+        this.curFloor = initialFloor;
+        this.doorOpen = true;//door must be open initially so people can get on
+        this.direction = Direction.IDLE;
+        this.floorOrder = new LinkedList<>();
+    }
+
+    //basic gets
+    public int getFloor(){
+        return curFloor;
+    }
+    public Direction getDirection(){
+        return direction;
+    }
+    public boolean isDoorOpen(){
+        return doorOpen;
+    }
+
+    public void openDoor(){
+        doorOpen = true;;
+        System.out.println("Floor " + getFloor());
+    }
+    public void closeDoor(){
+        doorOpen = false;
+        System.out.println("Stand clear of the closing doors!");
+    }
+
+    public void emergencyButton(){
+        System.out.println("CALLING 911 AND ALERTING MAINTENANCE!!");
+    }
+}
