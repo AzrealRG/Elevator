89801b88d451d36ef39740cadfbf5fadd2011cb5
AzrealRG <john.ha2006@gmail.com>
2025-10-21 16:06:52 -0400

    move method, direction change, create elevator

--- Elevator.java
+++ Elevator.java
@@ -2,7 +2,9 @@ import java.util.PriorityQueue;
 import java.util.Collections;
 
 public class Elevator{
-    private int curFloor;
+    private ElevatorNode<Integer> curFloor;
+    private ElevatorNode<Integer> topFloor;
+    private ElevatorNode<Integer> bottomFloor;
     private boolean doorOpen;
     private PriorityQueue<Integer> upOrder;
     private PriorityQueue<Integer> downOrder;
@@ -13,8 +15,8 @@ public class Elevator{
     }
 
     //creating the elevator only requires the initial floor
-    public Elevator(int initialFloor){
-        this.curFloor = initialFloor;
+    public Elevator(int numFloors){
+        buildElevator(numFloors);
         this.doorOpen = true;//door must be open initially so people can get on
         this.direction = Direction.IDLE;
         //max and min heaps to go next up or down floor.
@@ -22,9 +24,25 @@ public class Elevator{
         this.downOrder = new PriorityQueue<>(Collections.reverseOrder());
     }
 
+    private void buildElevator(int numFloors){
+        if (numFloors <= 1){//attempted to create a negative floor elevator
+            System.out.println("Number of Floors isn't positive");
+            return;
+        }
+        bottomFloor = curFloor = new ElevatorNode<Integer>(1);//holds bottom floor
+        ElevatorNode<Integer> prev = curFloor;
+        for(int i = 2; i <= numFloors; i++){
+            ElevatorNode<Integer> nextFloor = new ElevatorNode<Integer>(i);
+            prev.next = nextFloor;
+            nextFloor.prev = prev;
+            prev = prev.next;
+        }
+        topFloor = prev;//holds very top floor
+    }
+
     //basic gets
     public int getFloor(){
-        return curFloor;
+        return curFloor.data;
     }
     public Direction getDirection(){
         return direction;
@@ -53,6 +71,49 @@ public class Elevator{
         }
         else if(floor > getFloor()){
             upOrder.add(floor);
+        }
+    }
+
+    public void move(){
+        if(upOrder.isEmpty() && downOrder.isEmpty()){//trying to run move without any requests
+            System.out.println("No requested Floor");
+            direction = Direction.IDLE;
+            return;
+        }
+        else if(upOrder.isEmpty()){//switch to going down if no up requests
+            System.out.println("Going down");
+            direction = Direction.DOWN;
+        }
+        else{//switch to going up if no down requests
+            System.out.println("Going up");
+            direction = Direction.UP;
+        }
+
+        if(direction == Direction.UP){
+            int nextFloor = upOrder.poll();
+
+            if(topFloor.data < nextFloor){//next floor in queue doesn't exist
+                upOrder.clear();
+                System.out.println("No such floor!");
+            }
+
+            while (nextFloor > curFloor.data){
+                curFloor = curFloor.next;
+            }
+        }
+        else if(direction == Direction.DOWN){
+            int nextFloor = downOrder.poll();
+
+            if(bottomFloor.data > nextFloor){//next floor in queue doesn't exist
+                downOrder.clear();
+                System.out.println("No such floor!");
+                return;
+            }
+
+            while(nextFloor < curFloor.data){
+                curFloor = curFloor.prev;
+            }
+            System.out.println("Floor " + curFloor.data);
         }
     }
 }
